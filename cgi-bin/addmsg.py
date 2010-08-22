c831e46060381ae810ba983fa0c871730ca545e7
James Valleroy <james.valleroy@gmail.com>
2010-08-22 13:55:38 -0400

    python script to generate html form

--- cgi-bin/addmsg.py
+++ cgi-bin/addmsg.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python
-""" addmsg.py
-responds to askName.html
-and gives a customized greeting
-"""
-import cgi
 
-print "Content-type: text/html \n\n"
+# Import the CGI module
+import cgi
+import cgitb
+cgitb.enable()
 
 form = cgi.FieldStorage()
-userName = form["userName"].value
-print "<h1>Hi there, %s!</h1>" % userName
+
+# Print the required header that tells the browser how to render the text.
+print "Content-Type: text/html\n\n"
+
+print "<head>"
+print "<meta http-equiv=\"content-type\" content=\"text/xml; charset=utf-8\" />"
+
+print "<title>Free Speech Board</title>"
+print "</head>"
+
+print "<body>"
+print "<h1>Free Speech Board</h1>"
+print form["newmsg"].value
+print "</br></br>"
+print "<form action = \"addmsg.py\" method = \"get\">"
+print "  Message:</br>"
+print "  <textarea name=\"newmsg\" cols=60 rows=6></textarea></br>"
+print "  <input type=\"submit\" value=\"Submit\" />"
+print "</form>"
+print "</body>"
+print "</html>"
 
