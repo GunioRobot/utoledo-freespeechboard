e00c50c2697e275f8d6dd6b9e9ad8461ce7312fe
James Valleroy <james.valleroy@gmail.com>
2010-09-18 11:48:40 -0400

    Stores messages as individual documents in the db. Eventually we want to put multiple messages in one document, using a dict.

--- cgi-bin/addmsg.py
+++ cgi-bin/addmsg.py
@@ -1,11 +1,40 @@
 #!/usr/bin/python
 
+import time
+now = time.localtime()
+
+import couchdb
+#from couchdb.mapping import TextField, IntegerField, DateField, DictField
+
+# Ideal document format, not implemented yet.
+#class ThreadDoc(Document):
+#	subject = TextField()
+#	updated = DateTimeField(default=datetime.now)
+#	messages = DictField(Mapping.build(
+#		index = IntegerField(),
+#		messageItem = DictField(Mapping.build(
+#			timestamp = DateTimeField(default=datetime.now),
+#			messageText = TextField()
+#		))
+#	))
+
 # Import the CGI module
 import cgi
 import cgitb
 cgitb.enable()
 
 form = cgi.FieldStorage()
+
+if 'newmsg' in form:
+	couch = couchdb.Server()
+	if 'fsb-test' in couch:
+		db = couch['fsb-test']
+	else:
+		db = couch.create('fsb-test')
+
+	# Simplified code for testing:
+	doc = {'message': form["newmsg"].value}
+	db.create(doc)
 
 # Print the required header that tells the browser how to render the text.
 print "Content-Type: text/html\n\n"
@@ -18,14 +47,15 @@ print "</head>"
 
 print "<body>"
 print "<h1>Free Speech Board</h1>"
+
 if "newmsg" in form:
-	print form["newmsg"].value
-print "</br></br>"
-print "<form action = \"addmsg.py\" method = \"get\">"
-print "  Message:</br>"
-print "  <textarea name=\"newmsg\" cols=60 rows=6></textarea></br>"
-print "  <input type=\"submit\" value=\"Submit\" />"
-print "</form>"
+	print "<p>Added message: "
+	if "newmsg" in form:
+		print form["newmsg"].value
+	print "</p>"
+
+else:
+	print "<p>Error: No message</p>"
 print "</body>"
 print "</html>"
 
