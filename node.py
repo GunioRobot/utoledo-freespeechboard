9d94690d577f3589c7b6c995ee299908f95f4f2f
The Snark <michael.grube@gmail.com>
2010-10-02 09:26:32 -0400

    Moved couchdb server stuff to constructor. Started to get revision and address stuff in line...ran out of time!

--- node.py
+++ node.py
@@ -4,12 +4,19 @@ from twisted.application.internet import MulticastServer
 import couchdb
 
 class Node(DatagramProtocol):
+    
+    def __init__(self):
+	s = Server()
+	db = s['fsb-test']
+	
+
     #Join group 224.0.0.1
     def startProtocol(self):
         self.transport.joinGroup('224.0.0.1')
 
     # Multicast a version string
     def sendVersion(self, datagram, address):
+	myverstring = "Rev:" + db[_rev] + ";Address:" + address
         self.transport.write("Rev:12345;Address:123.456.78.910", address)
 
     def recvVersion(self, datagram, address):
