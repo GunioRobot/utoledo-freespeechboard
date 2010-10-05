a6f1d86f6605d93ca70d4325dc917cde115e0381
James Valleroy <james.valleroy@gmail.com>
2010-10-04 20:26:49 -0400

    cleaned up addmsg a little

--- cgi-bin/addmsg.py
+++ cgi-bin/addmsg.py
@@ -8,23 +8,11 @@ cgitb.enable()					# enable debugging
 
 form = cgi.FieldStorage()
 
-for thread in form:
-	couch = couchdb.Server()		# connect to server
-
-	if 'fsb-test' in couch:
-		db = couch['fsb-test']		# get database
-	else:
-		db = couch.create('fsb-test')	# or create new database
-
-	if thread in db:
-		doc = db[thread]		# get document
-		msgs = doc['msgs']		# get msgs dict
-
-		# add new message to working copy of document
-		msgs[str(len(msgs))] = {'timestamp': time.time(), 'message': form[thread].value}
-
-		doc['msgs'] = msgs		# store updated msgs in doc
-		db[thread] = doc		# store updated doc in db
+couch = couchdb.Server()		# connect to server
+if 'fsb-test' in couch:
+	db = couch['fsb-test']		# get database
+else:
+	db = couch.create('fsb-test')	# or create new database
 
 # Print the required header that tells the browser how to render the text.
 print "Content-Type: text/html\n\n"
@@ -38,17 +26,27 @@ print "</head>"
 print "<body>"
 print "<h1>Free Speech Board</h1>"
 
-foundThread = False
 for thread in form:
-	foundThread = True
+
 	if thread in db:
+		doc = db[thread]		# get document
+		msgs = doc['msgs']		# get msgs dict
+
+		# add new message to working copy of document
+		msgs[str(len(msgs))] = {'timestamp': time.time(), 'message': form[thread].value}
+
+		doc['msgs'] = msgs		# store updated msgs in doc
+		db[thread] = doc		# store updated doc in db
+
 		print "<p>Added message: "
 		print form[thread].value
 		print "</p>"
+
 	else:
-		print "<p>Error: Thread not found!</p>"
-if not foundThread:
-	print "<p>Error: No message</p>"
+		print "<p>Error: Thread"
+		print thread
+		print " not found!</p>"
+
 print "</body>"
 print "</html>"
 
