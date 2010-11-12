28ae53231480a7e63b89ed8589582aa18d24f06b
James Valleroy <james.valleroy@gmail.com>
2010-11-11 20:55:56 -0500

    Conflict merging in place but needs additional testing.

--- scripts/node.py
+++ scripts/node.py
@@ -7,6 +7,8 @@ import string
 import socket
 import fcntl
 import struct
+import pycurl
+import sys
 
 class Node(DatagramProtocol):    
 
@@ -32,6 +34,11 @@ class Node(DatagramProtocol):
 		self.myip = self.get_ip_address('wlan0')
 		self.s = couchdb.Server('http://' + self.myip + ':5984')
 		self.db = self.s['fsb-test']
+		self.contents = ''	# for pycurl
+
+
+	def body_callback(self, buf):	# for pycurl
+		self.contents = self.contents + buf
 	
 
 	#Join group 224.0.0.1
@@ -84,5 +91,61 @@ class Node(DatagramProtocol):
 		# check each document for conflicts
 		res = self.db.query(self.map_conflicts)
 		for row in res:
-			print row
+			# get revs for conflicting versions
+			curl = pycurl.Curl()
+			curl.setopt(curl.URL, 'http://' + self.myip + ':5984/fsb-test/' + row.id + '?open_revs=all')
+			curl.setopt(curl.WRITEFUNCTION, self.body_callback)
+			curl.perform()
+
+			index = self.contents.find('_id')
+			idnum1 = self.contents[index+6:index+38]
+			index = self.contents.find('_rev', index)
+			rev1 = self.contents[index+7:index+41]
+			index = self.contents.find('_id', index)
+			idnum2 = self.contents[index+6:index+38]
+			index = self.contents.find('_rev', index)
+			rev2 = self.contents[index+7:index+41]
+
+			# get conflicting documents
+			self.contents = ""
+			curl.setopt(curl.URL, 'http://' + self.myip + ':5984/fsb-test/' + idnum1 + '?rev='+rev1)
+			curl.perform()
+			doc1 = couchdb.json.decode(self.contents)
+			print doc1
+
+			self.contents = ""
+			curl.setopt(curl.URL, 'http://' + self.myip + ':5984/fsb-test/' + idnum2 + '?rev='+rev2)
+			curl.perform()
+			doc2 = couchdb.json.decode(self.contents)
+			print doc2
+
+			curl.close()
+
+			# merge documents
+			mergedoc = self.db[row.id]
+			msgdict = dict()
+			msgs = dict()
+			for msg in doc1['msgs']:
+				timestamp = str(doc1['msgs'][msg]['timestamp'])
+				msgdict[timestamp] = doc1['msgs'][msg]['message']
+			for msg in doc2['msgs']:
+				timestamp = str(doc2['msgs'][msg]['timestamp'])
+				msgdict[timestamp] = doc2['msgs'][msg]['message']
+			index = 0
+			for key in sorted(msgdict.iterkeys()):
+				msgs[index] = {'timestamp': key, 'message': msgdict[key]}
+				index = index + 1
+
+			updatetime = doc1['updatetime']
+			updatetime2 = doc2['updatetime']
+			if updatetime2 > updatetime:
+				updatetime = updatetime2
+
+			mergedoc['msgs'] = msgs
+			mergedoc['updatetime'] = updatetime
+			
+			# save merged document
+			self.db[row.id] = mergedoc
+			print 'Conflicts resolved!'
+
 
